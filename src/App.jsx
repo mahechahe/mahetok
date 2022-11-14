8c511256c4155ee8b8e841885a461fa28e55e8e8
Estiven Mahecha henao <mahechahe@gmail.com>
2022-11-13 23:53:25 -0500

    Add setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <main>
+
+        <h1>TikTok clone</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
